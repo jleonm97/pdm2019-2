b10e60d196570df8b7bcdc572ca5fa3147a0b35f
Sergio Villanueva Catacora <54380122+woozlak@users.noreply.github.com>
2019-10-11 15:39:16 -0500

    Update updatestyles.js

--- GUI_FRONT_END/js/updatestyles.js
+++ GUI_FRONT_END/js/updatestyles.js
@@ -1,26 +1,20 @@
 
-    var css;
-	var rotateX;
-	var rotateY;
-rotateX.value=0;
-rotateY.value=0;
-    const updateStyles = () => {
-      if (!css) {
+var css;
+var intervalID = setInterval(
+function(){
+    var angulo = $.getJSON( "http://10.101.58.62:3000/Prueba", function() {
+x=angulo.responseJSON.X + 45;
+y=angulo.responseJSON.Y;
+if (!css) {
         css = document.createElement('style');
         document.body.appendChild(css);
       }
       let cssText = `x-model {
-        transform: rotateX(${rotateX.value +45}deg) rotateY(${rotateY.value}deg) rotateZ(225deg);
+         transform: rotateX(${x}deg) rotateY(${y}deg) rotateZ(225deg);
       }`;
       css.textContent = cssText;
-    }
-  
-
-    updateStyles()
-    
-  //.wrap {
-  //      perspective: ${perspective.value}px;
-  //      perspective-origin: ${perspectiveOriginX.value}% ${perspectiveOriginY.value}%;
-  //    }*/
-  
-  //http://192.168.10.10:3000
+})
+  .fail(function() {
+    alert( "error: no se recibe angulos" );
+  });
+ },1000);
